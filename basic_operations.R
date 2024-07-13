55bcfb74fe47c0bf6061fdb33b8d9d6d8e320584
Flavio Yuan <fyuangouvea@gmail.com>
2024-07-12 23:47:13 -0300

    Add - filtering iris dataset by Species == setosa. Two methods: Basic R and using dplyr

--- basic_operations.R
+++ basic_operations.R
@@ -41,3 +41,19 @@ chronos <- c(10.60, 10.12, 9.58, 11.1)
 
 head(datasets::iris)
 summary(datasets::iris)
+
+# get all Sepal length of setosa Species using Basic R
+setosa_sepal_lengths <- iris[iris$Species == "setosa", "Sepal.Length"]
+
+# get all Sepal length of setosa Species using dplyr package. By chatGPT
+
+# Install and load the dplyr package if not already installed
+# install.packages("dplyr")
+library(dplyr)
+
+# Filter the iris dataset to get Sepal.Length where Species is "setosa"
+setosa_sepal_lengths_dplyr <- iris %>%
+    filter(Species == "setosa") %>%
+    select(Sepal.Length)
+
+print(setosa_sepal_lengths_dplyr)
